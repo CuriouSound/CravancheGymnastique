d82d6c8afd16ba6bf9388e1479f22d32a93556c0
JauneLoke <jaune.loke@gmail.com>
2019-02-18 11:11:04 +0100

    Ajout git ignore

---
+++ CravancheAdmin/gym/src/Entity/Auteur.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AuteurRepository")
+ */
+class Auteur
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Actualite", mappedBy="auteur")
+     */
+    private $actualites;
+
+    public function __construct()
+    {
+        $this->actualites = new ArrayCollection();
+    }
+
+    public function __toString()
+    {
+        return $this->name.$this->id;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Actualite[]
+     */
+    public function getActualites(): Collection
+    {
+        return $this->actualites;
+    }
+
+    public function addActualite(Actualite $actualite): self
+    {
+        if (!$this->actualites->contains($actualite)) {
+            $this->actualites[] = $actualite;
+            $actualite->setAuteur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeActualite(Actualite $actualite): self
+    {
+        if ($this->actualites->contains($actualite)) {
+            $this->actualites->removeElement($actualite);
+            // set the owning side to null (unless already changed)
+            if ($actualite->getAuteur() === $this) {
+                $actualite->setAuteur(null);
+            }
+        }
+
+        return $this;
+    }
+}
